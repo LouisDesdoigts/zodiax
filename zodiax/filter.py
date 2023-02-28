7811f16103895e0b7e3731d7fe58666b7cc64c32
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2023-02-28 17:50:17 +1100

    Adding basic type annotations and doccumentation

--- zodiax/filter.py
+++ zodiax/filter.py
@@ -1,5 +1,7 @@
 import zodiax
 from functools import wraps
+from jaxtyping import Array, PyTree
+from typing import Union, Callable, Any
 
 # Wrapped imports
 from equinox import partition, combine, \
@@ -18,23 +20,60 @@ __all__ = ["filter_grad", "filter_value_and_grad", "filter_jit",
            "filter_closure_convert", "filter_vmap", "filter_pmap", 
            "filter_pure_callback"]
 
-def convert_to_filter(pytree, params):
+def convert_to_filter(pytree : PyTree, 
+                      params : Union[PyTree, list, str]) -> PyTree:
+    """
+    Converts the input params to a filter spec if not already.
+
+    Parameters
+    ----------
+    pytree : PyTree
+        The pytree to filter.
+    params : Union[PyTree, list, str]
+        The params to filter, or an existing filter.
+    
+    Returns
+    -------
+     : PyTree
+        The filter spec.
+    """
     if isinstance(params, zodiax.base.ExtendedBase):
         return params
     else:
         return pytree.get_args(params)
 
-def filter_grad(params, *filter_args, **filter_kwargs):
-    def wrapper(func):
+def filter_grad(params : Union[PyTree, list, str], 
+                *filter_args, **filter_kwargs) -> Callable:
+    """
+    Applies the equinox filter_grad function to the input params.
+
+    Parameters
+    ----------
+    params : Union[PyTree, list, str]
+        The params to filter. Can either be a single string path, a list of 
+        paths, or a pytree with binary leaves denoting which argument to take
+        gradients with respect to.
+    *filter_args : Any
+        The args to pass to the equinox filter_grad function.
+    **filter_kwargs : Any
+        The kwargs to pass to the equinox filter_grad function.
+    
+    Returns
+    -------
+     : Callable
+        The wrapped function.
+    """
+    def wrapper(func : Callable):
+        
         @wraps(func)
-        def inner_wrapper(pytree, *args, **kwargs):
+        def inner_wrapper(pytree : PyTree, *args, **kwargs):
 
             # Convert params to filter spec if not already
             filter_spec = convert_to_filter(pytree, params)
 
             # Wrap original function
             @fgrad(*filter_args, **filter_kwargs)
-            def recombine(diff, non_diff):
+            def recombine(diff : PyTree, non_diff : PyTree):
                 return func(combine(diff, non_diff), *args, **kwargs)
             
             # Return wrapped function
@@ -42,17 +81,38 @@ def filter_grad(params, *filter_args, **filter_kwargs):
         return inner_wrapper
     return wrapper
 
-def filter_value_and_grad(params, *filter_args, **filter_kwargs):
-    def wrapper(func):
+def filter_value_and_grad(params : Union[PyTree, list, str], 
+                *filter_args, **filter_kwargs) -> Callable:
+    """
+    Applies the equinox filter_value_and_grad function to the input params.
+
+    Parameters
+    ----------
+    params : Union[PyTree, list, str]
+        The params to filter. Can either be a single string path, a list of 
+        paths, or a pytree with binary leaves denoting which argument to take
+        gradients with respect to.
+    *filter_args : Any
+        The args to pass to the equinox filter_value_and_grad function.
+    **filter_kwargs : Any
+        The kwargs to pass to the equinox filter_value_and_grad function.
+    
+    Returns
+    -------
+     : Callable
+        The wrapped function.
+    """
+    def wrapper(func : Callable):
+
         @wraps(func)
-        def inner_wrapper(pytree, *args, **kwargs):
+        def inner_wrapper(pytree : PyTree, *args, **kwargs):
 
             # Convert params to filter spec if not already
             filter_spec = convert_to_filter(pytree, params)
 
             # Wrap original function
             @fvgrad(*filter_args, **filter_kwargs)
-            def recombine(diff, non_diff):
+            def recombine(diff : PyTree, non_diff : PyTree):
                 return func(combine(diff, non_diff), *args, **kwargs)
             
             # Return wrapped function
