b11f04fe87c0d39ab9ea08187c7d2c98a4e4c736
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2023-03-01 13:00:02 +1100

    Add the simple filter function wrappers

--- zodiax/filter.py
+++ zodiax/filter.py
@@ -1,17 +1,30 @@
 import zodiax
 from functools import wraps
-from jaxtyping import Array, PyTree
-from typing import Union, Callable, Any
+from jaxtyping import PyTree
+from typing import Union, Callable
 
-# Wrapped imports
-from equinox import partition, combine, \
-    filter_grad as fgrad, \
-    filter_value_and_grad as fvgrad
+# # Wrapped imports
+# from equinox import partition, combine, \
+#     filter_grad as fgrad, \
+#     filter_value_and_grad as fvgrad
     
-# Unwrapped import
-from equinox import filter_jit, filter_make_jaxpr, filter_eval_shape, \
-    filter_jvp, filter_vjp, filter_custom_jvp, filter_custom_vjp, \
-    filter_closure_convert, filter_vmap, filter_pmap, filter_pure_callback
+# # Unwrapped import
+# from equinox import filter_jit, filter_make_jaxpr, filter_eval_shape, \
+#     filter_jvp, filter_vjp, filter_custom_jvp, filter_custom_vjp, \
+#     filter_closure_convert, filter_vmap, filter_pmap, filter_pure_callback
+
+from equinox import partition, combine
+from equinox import ad
+from equinox import jit
+from equinox import make_jaxpr
+from equinox import vmap_pmap
+from equinox import callback
+from equinox import eval_shape
+
+# # Unwrapped import
+# from equinox import filter_jit as fj, filter_make_jaxpr, filter_eval_shape, \
+#     filter_jvp, filter_vjp, filter_custom_jvp, filter_custom_vjp, \
+#     filter_closure_convert, filter_vmap, filter_pmap, filter_pure_callback
 
 
 __all__ = ["filter_grad", "filter_value_and_grad", "filter_jit", 
@@ -42,6 +55,7 @@ def convert_to_filter(pytree : PyTree,
     else:
         return pytree.get_args(params)
 
+# Autodiff
 def filter_grad(params : Union[PyTree, list, str], 
                 *filter_args, **filter_kwargs) -> Callable:
     """
@@ -75,7 +89,7 @@ def filter_grad(params : Union[PyTree, list, str],
             filter_spec = convert_to_filter(pytree, params)
 
             # Wrap original function
-            @fgrad(*filter_args, **filter_kwargs)
+            @ad.filter_grad(*filter_args, **filter_kwargs)
             def recombine(diff : PyTree, non_diff : PyTree):
                 return func(combine(diff, non_diff), *args, **kwargs)
             
@@ -83,6 +97,7 @@ def filter_grad(params : Union[PyTree, list, str],
             return recombine(*partition(pytree, filter_spec))
         return inner_wrapper
     return wrapper
+
 
 def filter_value_and_grad(params : Union[PyTree, list, str], 
                 *filter_args, **filter_kwargs) -> Callable:
@@ -116,11 +131,170 @@ def filter_value_and_grad(params : Union[PyTree, list, str],
             filter_spec = convert_to_filter(pytree, params)
 
             # Wrap original function
-            @fvgrad(*filter_args, **filter_kwargs)
+            @ad.filter_value_and_grad(*filter_args, **filter_kwargs)
             def recombine(diff : PyTree, non_diff : PyTree):
                 return func(combine(diff, non_diff), *args, **kwargs)
             
             # Return wrapped function
             return recombine(*partition(pytree, filter_spec))
         return inner_wrapper
+    return wrapper
+
+
+@wraps(ad.filter_jvp)
+def filter_jvp(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_jvp`. The corresponding 
+    equinox docs are found [here](https://docs.kidger.site/equinox/api/
+    filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return ad.filter_jvp(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
+    return wrapper
+
+
+@wraps(ad.filter_vjp)
+def filter_vjp(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_vjp`. The corresponding 
+    equinox docs are found [here](https://docs.kidger.site/equinox/api/
+    filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return ad.filter_vjp(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
+    return wrapper
+
+
+@wraps(ad.filter_closure_convert)
+def filter_closure_convert(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_closure_convert`. The 
+    corresponding equinox docs are found [here](https://docs.kidger.site/
+    equinox/api/filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return ad.filter_closure_convert(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
+    return wrapper
+
+
+@wraps(ad.filter_custom_vjp)
+def filter_custom_vjp(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_custom_vjp`. The corresponding 
+    equinox docs are found [here](https://docs.kidger.site/equinox/api/
+    filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return ad.filter_custom_vjp(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
+    return wrapper
+
+
+@wraps(ad.filter_custom_jvp)
+def filter_custom_jvp(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_custom_jvp`. The corresponding 
+    equinox docs are found [here](https://docs.kidger.site/equinox/api/
+    filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return ad.filter_custom_jvp(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
+    return wrapper
+
+
+# Jit
+@wraps(jit.filter_jit)
+def filter_jit(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_jit`. The corresponding 
+    equinox docs are found [here](https://docs.kidger.site/equinox/api/
+    filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return jit.filter_jit(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
+    return wrapper
+
+
+# Callback
+@wraps(callback.filter_pure_callback)
+def filter_pure_callback(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_pure_callback`. The 
+    corresponding equinox docs are found [here](https://docs.kidger.site/
+    equinox/api/filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return callback.filter_pure_callback(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
+    return wrapper
+
+
+# Eval shape
+@wraps(eval_shape.filter_eval_shape)
+def filter_eval_shape(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_eval_shape`. The corresponding 
+    equinox docs are found [here](https://docs.kidger.site/equinox/api/
+    filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return eval_shape.filter_eval_shape(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
+    return wrapper
+
+
+# Vmap Pmap
+@wraps(vmap_pmap.filter_vmap)
+def filter_vmap(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_vmap`. The corresponding 
+    equinox docs are found [here](https://docs.kidger.site/equinox/api/
+    filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return vmap_pmap.filter_vmap(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
+    return wrapper
+
+
+@wraps(vmap_pmap.filter_pmap)
+def filter_pmap(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_pmap`. The corresponding 
+    equinox docs are found [here](https://docs.kidger.site/equinox/api/
+    filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return vmap_pmap.filter_pmap(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
+    return wrapper
+
+
+# Make Jaxpr
+@wraps(make_jaxpr.filter_make_jaxpr)
+def filter_make_jaxpr(func, *filter_args, **filter_kwargs):
+    """
+    A simple wrapper fucntion for `equinox.filter_make_jaxpr`. The corresponding 
+    equinox docs are found [here](https://docs.kidger.site/equinox/api/
+    filtering/transformations/)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return make_jaxpr.filter_make_jaxpr(func, 
+                              *filter_args, **filter_kwargs)(*args, **kwargs)
     return wrapper
