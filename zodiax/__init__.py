a8c203eb7dee1479f6ae758e0d8b278626004cbc
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2023-03-06 19:34:15 +1100

    Adding serialisation methods

--- zodiax/__init__.py
+++ zodiax/__init__.py
@@ -4,10 +4,12 @@ __version__ = "0.2.0"
 # Import as modules
 from . import base
 from . import filter
+from . import serialisation
 
 # Import core functions from modules
 from .base import *
 from .filter import *
+from .serialisation import *
 
 # Add to __all__
-__all__ = base.__all__, filter.__all__
+__all__ = base.__all__, filter.__all__, serialisation.__all__
