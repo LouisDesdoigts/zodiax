5e938952656613a4e9f58f66e70d30dcd3c7bd1c
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2023-03-07 13:58:55 +1100

    Replacing commas with plusses in the __all__ list

--- zodiax/__init__.py
+++ zodiax/__init__.py
@@ -12,4 +12,4 @@ from .filter import *
 from .serialisation import *
 
 # Add to __all__
-__all__ = base.__all__, filter.__all__, serialisation.__all__
+__all__ = base.__all__ + filter.__all__ + serialisation.__all__
