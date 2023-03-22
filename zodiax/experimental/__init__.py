8d1c02d09a05ed6aa698724253dfdb9c846f162e
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2023-03-22 12:44:48 +1100

    Adding 'get_jit_model' to experimental. Fixes #4

--- zodiax/experimental/__init__.py
+++ zodiax/experimental/__init__.py
@@ -1,11 +1,12 @@
-# name = "experimental"
-name = "dev"
+name = "experimental"
 
 # Import as modules
 from . import serialisation
+from . import jit
 
 # Import core functions from modules
 from .serialisation import *
+from .jit import *
 
 # Add to __all__
-__all__ = serialisation.__all__
+__all__ = serialisation.__all__ + jit.__all__
