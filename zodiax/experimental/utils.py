1bb7e43261cd7d8a5d99cf1e9ffaec1c3e76a928
maxecharles <max.charles@sydney.edu.au>
2025-01-14 11:34:45 +1100

    Making Ben's code readable

--- zodiax/experimental/utils.py
+++ zodiax/experimental/utils.py
@@ -1,34 +1,59 @@
-import numpy as np 
+from jax import numpy as np, Array
 import scipy.cluster.hierarchy as sch
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-def cov_to_corr(cov):
+
+def cov_to_corr(cov: Array) -> Array:
+    """
+    Calculate the correlation matrix from the covariance matrix.
+    """
+
     # Calculate the standard deviations
-    std = np.sqrt(np.diag(cov))
+    stds = np.sqrt(np.diag(cov))
 
     # Calculate the correlation matrix
-    corr = cov / np.outer(std, std)
+    return cov / np.outer(stds, stds)
 
-    return corr
 
-def group_corr_inds(corr):
+def get_group_inds(corr: Array) -> Array:
+    """
+    Group indices of correlated variables.
+    """
+    # Compute the distance between each pair of variables
+    d = sch.distance.pdist(corr)  # vector of ('55' choose 2) pairwise distances
 
-    # print(corr)
-    # X = df.corr().values
-    X = corr 
-    # print(X.shape)
-    d = sch.distance.pdist(X)   # vector of ('55' choose 2) pairwise distances
-    L = sch.linkage(d, method='complete')
-    ind = sch.fcluster(L, 0.5*d.max(), 'distance')
+    # Perform hierarchical/agglomerative clustering
+    L = sch.linkage(d, method="complete")
+
+    # Given a distance threshold, return indices of clusters
+    # i.e. array of cluster indices corresponding to each variable
+    ind = sch.fcluster(L, 0.5 * d.max(), "distance")
+
     return ind
 
-def visualize_covmat(cov,plot_names):
+
+def plot_corr_matrix(cov: Array, param_labels: list):
+    """
+    Plot the sorted correlation matrix for a given covariance matrix.
+
+    Args:
+    cov: Array
+        Covariance matrix.
+    params: list
+        List of parameter names (strings).
+    """
+
+    # Convert covariance matrix to correlation matrix
     corr = cov_to_corr(cov)
-    ind = group_corr_inds(corr)
-    
+
+    # Sorting
+    ind = get_group_inds(corr)
     args = np.argsort(ind)
-    sorted_labels = [plot_names[i] for i in args]
+    sorted_labels = [param_labels[i] for i in args]
+
+    # Plotting
     plt.imshow(corr[args, :][:, args], origin="upper")
-    plt.xticks(range(len(plot_names)), sorted_labels, rotation=45)
-    plt.yticks(range(len(plot_names)), sorted_labels)
+    plt.xticks(range(len(param_labels)), sorted_labels, rotation=45)
+    plt.yticks(range(len(param_labels)), sorted_labels)
     plt.colorbar()
+    plt.show()
