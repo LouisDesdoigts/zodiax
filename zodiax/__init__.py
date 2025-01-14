3ec9dde4e5ff86b367ff9807afdbfff1a532eaa2
maxecharles <max.charles@sydney.edu.au>
2025-01-14 15:11:09 +1100

    adding back imports into top-level namespace

--- zodiax/__init__.py
+++ zodiax/__init__.py
@@ -1,22 +1,26 @@
 # Import as modules
 from . import (
     base,
-    tree,
-    optimisation,
-    experimental,
-    eqx,
     bayes,
+    eqx,
+    optimisation,
+    tree,
+    experimental,
 )
 
 name = "zodiax"
 __version__ = "0.4.2"
 
+# Dynamically import symbols into the top-level namespace
+for module in [base, bayes, eqx, optimisation, tree]:
+    globals().update({name: getattr(module, name) for name in module.__all__})
+
 # Add to __all__
 __all__ = (
     base.__all__
-    + tree.__all__
-    + optimisation.__all__
-    + experimental.__all__
-    + eqx.__all__
     + bayes.__all__
+    + eqx.__all__
+    + optimisation.__all__
+    + tree.__all__
+    + experimental.__all__
 )
