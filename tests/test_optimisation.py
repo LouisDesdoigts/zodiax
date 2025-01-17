8bfc19ef186489a965ed9683f67317aa0a91d325
maxecharles <max.charles@sydney.edu.au>
2025-01-17 18:02:05 +1100

    fixing tests. i will never learn

--- tests/test_optimisation.py
+++ tests/test_optimisation.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import zodiax
 from jax import config
+import optax
 
 config.update("jax_debug_nans", True)
 
@@ -55,6 +56,32 @@ def test_get_optimiser(create_base):
     # Define parameters and construct base
     pytree = create_base()
 
+    # Paths
+    paths = [
+        "param",
+        "b.param",
+        ["param", "b.param"],
+    ]
+
+    # Optimisers
+    optimisers = [
+        optax.adam(0),
+        optax.adam(1),
+        [optax.adam(0), optax.adam(1)],
+    ]
+
+    # Test paths
+    for path, optimiser in zip(paths, optimisers):
+        zodiax.optimisation.get_optimiser(pytree, path, optimiser)
+
+
+def test_get_model_params_optimiser(create_base):
+    """
+    tests the get_optimiser method
+    """
+    # Define parameters and construct base
+    pytree = create_base()
+
     # Optimisers
     optimisers = {
         "param": zodiax.optimisation.sgd(0, 1, (1, 0.5), (2, 0.25)),
@@ -62,17 +89,4 @@ def test_get_optimiser(create_base):
     }
 
     # Test paths
-    zodiax.optimisation.get_optimiser(pytree, optimisers)
-
-
-# NOTE: This test is for the old version
-# def test_get_optimiser(create_base):
-#     """
-#     tests the get_optimiser method
-#     """
-#     # Define parameters and construct base
-#     pytree = create_base()
-
-#     # Test paths
-#     for path, optimiser in zip(paths, optimisers):
-#         zodiax.optimisation.get_optimiser(pytree, path, optimiser)
+    zodiax.optimisation.get_model_params_optimiser(pytree, optimisers)
