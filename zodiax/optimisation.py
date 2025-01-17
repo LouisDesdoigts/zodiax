452296b1d145bf260e8d8f619d1b50897b06055f
maxecharles <max.charles@sydney.edu.au>
2025-01-17 17:44:25 +1100

    adding back in old get_optimiser, renaming amigo version to 'get_model_params_optimiser'

--- zodiax/optimisation.py
+++ zodiax/optimisation.py
@@ -148,7 +148,64 @@ def zero_nan_check(grads: Base()) -> Base():
     return jax.tree.map(lambda x: np.where(np.isnan(x), 0.0, x), grads)
 
 
-def get_optimiser(pytree: Base(), optimisers: dict, parameters: Params = None):
+# NOTE old version, consider if its worth keeping
+def get_optimiser(
+    pytree: Base(),
+    parameters: Params,
+    optimisers: Optimisers,
+) -> tuple:
+    """
+    Returns an Optax.GradientTransformion object, with the optimisers
+    specified by optimisers applied to the leaves specified by parameters.
+
+    Parameters
+    ----------
+    pytree : Base
+        A zodiax.base.Base object.
+    parameters :  Union[str, List[str]]
+        A path or list of parameters or list of nested parameters.
+    optimisers : Union[optax.GradientTransformation, list]
+        A optax.GradientTransformation or list of
+        optax.GradientTransformation objects to be applied to the leaves
+        specified by parameters.
+
+    Returns
+    -------
+    optimiser : optax.GradientTransformion
+        TODO Update
+        A tuple of (Optax.GradientTransformion, optax.MultiTransformState)
+        objects, with the optimisers applied to the leaves specified by
+        parameters, and the initialised optimisation state.
+    state : optax.MultiTransformState
+    """
+    # Pre-wrap single inputs into a list since optimisers have a length of 2
+    if not isinstance(optimisers, list):
+        optimisers = [optimisers]
+
+    # parameters have to default be wrapped in a list to match optimiser
+    if isinstance(parameters, str):
+        parameters = [parameters]
+
+    # Construct groups and get param_spec
+    groups = [str(i) for i in range(len(optimisers))]
+    param_spec = jax.tree.map(lambda _: "null", pytree)
+    param_spec = param_spec.set(parameters, groups)
+
+    # Generate optimiser dictionary and Assign the null group
+    opt_dict = dict([(groups[i], optimisers[i]) for i in range(len(groups))])
+    opt_dict["null"] = optax.adam(0.0)
+
+    # Get optimiser object and filtered optimiser
+    optim = optax.multi_transform(opt_dict, param_spec)
+    opt_state = optim.init(eqx.filter(pytree, eqx.is_array))
+
+    # Return
+    return (optim, opt_state)
+
+
+def get_model_params_optimiser(
+    pytree: Base(), optimisers: dict, parameters: Params = None
+):
     """
     Returns an Optax.GradientTransformion object, with the optimisers
     specified by optimisers applied to the leaves specified by parameters.
@@ -189,58 +246,3 @@ def get_optimiser(pytree: Base(), optimisers: dict, parameters: Params = None):
     # Build the optimised object - the 'model_params' object
     state = optim.init(model_params)
     return model_params, optim, state
-
-
-# NOTE old version, consider if its worth keeping
-# def get_optimiser(
-#     pytree: Base(),
-#     parameters: Params,
-#     optimisers: Optimisers,
-# ) -> tuple:
-#     """
-#     Returns an Optax.GradientTransformion object, with the optimisers
-#     specified by optimisers applied to the leaves specified by parameters.
-
-#     Parameters
-#     ----------
-#     pytree : Base
-#         A zodiax.base.Base object.
-#     parameters :  Union[str, List[str]]
-#         A path or list of parameters or list of nested parameters.
-#     optimisers : Union[optax.GradientTransformation, list]
-#         A optax.GradientTransformation or list of
-#         optax.GradientTransformation objects to be applied to the leaves
-#         specified by parameters.
-
-#     Returns
-#     -------
-#     optimiser : optax.GradientTransformion
-#         TODO Update
-#         A tuple of (Optax.GradientTransformion, optax.MultiTransformState)
-#         objects, with the optimisers applied to the leaves specified by
-#         parameters, and the initialised optimisation state.
-#     state : optax.MultiTransformState
-#     """
-#     # Pre-wrap single inputs into a list since optimisers have a length of 2
-#     if not isinstance(optimisers, list):
-#         optimisers = [optimisers]
-
-#     # parameters have to default be wrapped in a list to match optimiser
-#     if isinstance(parameters, str):
-#         parameters = [parameters]
-
-#     # Construct groups and get param_spec
-#     groups = [str(i) for i in range(len(optimisers))]
-#     param_spec = tree_map(lambda _: "null", pytree)
-#     param_spec = param_spec.set(parameters, groups)
-
-#     # Generate optimiser dictionary and Assign the null group
-#     opt_dict = dict([(groups[i], optimisers[i]) for i in range(len(groups))])
-#     opt_dict["null"] = adam(0.0)
-
-#     # Get optimiser object and filtered optimiser
-#     optim = multi_transform(opt_dict, param_spec)
-#     opt_state = optim.init(eqx_filter(pytree, is_array))
-
-#     # Return
-#     return (optim, opt_state)
