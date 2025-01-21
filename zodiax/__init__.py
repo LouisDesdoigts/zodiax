9a0468e662f6e9dd0e27809b0bb1862559be40dc
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2025-01-21 14:58:46 +1100

    Adding wrappers docs and tests, few small other fixed (#46)
    * Adding wrappers docs and tests, few small typo fixes
    
    * Adding set_array call to transform functions

--- zodiax/__init__.py
+++ zodiax/__init__.py
@@ -5,6 +5,7 @@ from . import (
     fisher,
     optimisation,
     tree,
+    wrappers,
     experimental,
 )
 
@@ -12,7 +13,7 @@ name = "zodiax"
 __version__ = "0.5.0"
 
 # Dynamically import symbols into the top-level namespace
-for module in [base, fisher, eqx, optimisation, tree]:
+for module in [base, fisher, eqx, optimisation, tree, wrappers]:
     globals().update({name: getattr(module, name) for name in module.__all__})
 
 # Add to __all__
@@ -22,5 +23,6 @@ __all__ = (
     + eqx.__all__
     + optimisation.__all__
     + tree.__all__
+    + wrappers.__all__
     + experimental.__all__
 )
--- zodiax/wrappers.py
+++ zodiax/wrappers.py
@@ -1,4 +1,4 @@
-import zodiax as zdx
+import zodiax
 import equinox as eqx
 import jax
 from jax import numpy as np
@@ -9,6 +9,25 @@ __all__ = ["build_wrapper", "EquinoxWrapper", "WrapperHolder"]
 
 
 def build_wrapper(eqx_model, filter_fn=eqx.is_array):
+    """
+    Deconstructs an equinox model into its values and structure, and returns a
+    `WrapperHolder` object that can be used to interact with the model in a way
+    that is compatible with the Zodiax framework.
+
+    Parameters
+    ----------
+    eqx_model : equinox.Module
+        The Equinox model to deconstruct.
+    filter_fn : callable, optional
+        A function that takes a leaf of the model and returns a boolean value
+
+    Returns
+    -------
+    values : np.ndarray
+        The values of the model, flattened and concatenated.
+    structure : EquinoxWrapper
+        The structure of the model, stored in a `EquinoxWrapper` object.
+    """
     arr_mask = jax.tree.map(lambda leaf: filter_fn(leaf), eqx_model)
     dyn, static = eqx.partition(eqx_model, arr_mask)
     leaves, tree_def = jax.tree.flatten(dyn)
@@ -16,7 +35,17 @@ def build_wrapper(eqx_model, filter_fn=eqx.is_array):
     return values, EquinoxWrapper(static, leaves, tree_def)
 
 
-class EquinoxWrapper(zdx.Base):
+class EquinoxWrapper(zodiax.base.Base):
+    """
+    A wrapper class designed to store an Equinox model (typically a neural network)
+    in a way that makes it easily compatible within the Zodiax framework. This is
+    necessary as Equinox operates on _whole_ models, where as Zodiax operates on
+    model _leaves_. This class is designed to bridge that gap.
+
+    This class should not need to be interacted with directly, and is designed to be
+    held within the `WrapperHolder` class.
+    """
+
     static: eqx.Module
     shapes: list
     sizes: list
@@ -38,12 +67,64 @@ class EquinoxWrapper(zdx.Base):
         return eqx.combine(jax.tree.unflatten(self.tree_def, leaves), self.static)
 
 
-class WrapperHolder(zdx.Base):
+class WrapperHolder(zodiax.base.Base):
+    """
+    A class designed to hold an Equinox model, its structure and values. This helps it
+    operate smoothly within the Zodiax framework.
+
+    To apply transformations to the Equinox model values, operate on the `values` leaf
+    of this class. To build the model, call the `build` property, and the Equinox model
+    will be constructed with the stored values and be able to operated with as if it
+    were a regular Equinox model.
+
+    This class is designed to be instantiated by the `build_wrapper` function.
+
+    Example
+    -------
+
+    ```python
+    import equinox as eqx
+    import zodiax as zdx
+    import jax.numpy as np
+    import jax.random as jr
+
+    eqx_model = eqx.nn.MLP(
+        in_size=16, out_size=16, width_size=32, depth=1, key=jr.PRNGKey(0)
+    )
+
+    class Foo(zdx.WrapperHolder):
+
+        def __init__(self, nn):
+            values, structure = zdx.build_wrapper(nn)
+            self.values = values
+            self.structure = structure
+
+        def __call__(self, x):
+            return self.build(x)
+
+    x = np.ones(16)
+    foo = Foo(eqx_model)
+
+    # Now we can use the model as if it were a regular Equinox model
+    print(foo(x))
+
+    >>> [ 0.1767296   0.15628047 -0.63250038 -0.01583058  0.39692974  0.4556041
+    >>>   0.33121592 -0.3183221  -0.75008567 -0.32724514  0.28351735 -0.03595607
+    >>>  -0.53921278 -0.20966474 -0.33641739 -0.28726151]
+
+    # We can also apply Zodiax transformations to the model!
+    print(foo.multiply("values", 0.)(x))
+    >>> [0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+    """
+
     values: np.ndarray
     structure: EquinoxWrapper
 
     @property
     def build(self):
+        """
+        Builds the Equinox model with the stored values and structure.
+        """
         return self.structure.inject(self.values)
 
     def __getattr__(self, name):
